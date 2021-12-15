d78d4e1c168c7b2c218ddaaa257f173db3f0dfd2
Matias Gultig <magontek@gmail.com>
2021-12-15 17:55:01 -0300

    corregi el espaciado en la ayuda

--- commands/ayuda.js
+++ commands/ayuda.js
@@ -7,21 +7,19 @@ module.exports = {
 		.setDescription('Ayuda del juego'),
 	async execute(interaction,game) {
 
-        return interaction.reply({content: `
-        Para crear un personaje use:\n
-        /nuevapartida nombre [Nombre del personaje]\n
-        /nuevapartida historia [Elija un nombre de historia valido]\n
-        /nuevapartida clase [Elija una clase valida de personaje]\n
-\n
-        Para listar las clases validas primero asigne un nombre de historia y luego use\n
-        /listarclases\n
-\n
-        Para comenzar la partida use:\n
-        /continuarpartida\n
-\n
-        La hoja de personaje no se actualiza automaticamente\n
-        Para actualizarla usar:\n
-        /personaje
-        `, ephemeral: true})
+        return interaction.reply({content: `Para crear un personaje use:
+        /nuevapartida nombre [Nombre del personaje]
+        /nuevapartida historia [Elija un nombre de historia valido]
+        /nuevapartida clase [Elija una clase valida de personaje]
+
+        Para listar las clases validas primero asigne un nombre de historia y luego use
+        /listarclases
+
+        Para comenzar la partida use:
+        /continuarpartida
+
+        La hoja de personaje no se actualiza automaticamente
+        Para actualizarla usar:
+        /personaje`, ephemeral: true})
 	},
 };
